f476ecfeb63d7d33200c3147a3360065effc1d65
Sucharitha <Sucharitha@LAPTOP-6TJFU0SC>
2022-12-23 15:03:29 +0530

    welcome to task

---
+++ testproject1/src/testproject1/drivercls.java
@@ -0,0 +1,15 @@
+package testproject1;
+
+public class drivercls {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		objectcls m;
+		m=new objectcls();
+		m.display();
+		
+
+	}
+
+}
---
+++ testproject1/src/testproject1/objectcls.java
@@ -0,0 +1,10 @@
+package testproject1;
+
+public class objectcls {
+	void display() {
+		System.out.println("welcome to task");
+	
+		
+	}
+
+}
